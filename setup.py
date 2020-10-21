b91347ac63c25a3b07ceadbb9c15f86d63a2d907
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-10-21 15:07:51 +0300

    Update version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ entry_points = {"console_scripts": ["fpx = fpx.cli:fpx"]}
 
 setup(
     name="fpx",
-    version="0.1.3",
+    version="0.1.4",
     description="""""",
     long_description=long_description,
     author="Sergey Motornyuk",
